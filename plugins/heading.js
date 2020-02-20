0e7a63838db5ed5d7b1de2853bf0917a4489ac5b
qtvb1987 <qtvb1987@163.com>
2020-02-20 16:05:44 +0800

    提交

---
+++ plugins/heading.js
@@ -0,0 +1,56 @@
+
+//插件需要实现install
+const MyPlugin = {
+    install(Vue,options){
+       //heading组件
+           //<heading :level="1" :title="title" icon="cart">{{title}} </heading>
+           //<h2 title=""><svg><use xlink:href="#icon-cart"></use></svg></h2>
+           Vue.component('heading',{
+            props:{
+                //level:String
+                level:{
+                    type:String,
+                    required:true
+                },
+                title:{
+                    type:String,
+                    default:''
+                },
+                icon:{
+                    type:String
+                }
+            },
+            render(h){
+                //子节点数组
+                let children=[];
+
+                //icon 属性处理逻辑
+                if(this.icon){
+                    //<svg class="icon"><use xlink:href="#icon-cart"></use></svg>
+                    children.push(h(
+                        'svg',
+                        {class:'icon'},
+                        [h('use',{attrs:{'xlink:href':'#icon-'+this.icon}})]
+                        ))
+                }
+                //拼接子节点
+                children = children.concat(this.$slots.default)
+                const vnode =h(
+                    'h'+this.level,//参数1：tagname
+                    {attrs:{title:this.title}},//参数2:{...}
+                    children//参数3：子节点VNode数组
+                )
+                console.log(vnode);
+                //返回createElement返回的Vnode
+                //h---snabbdom
+               return vnode
+            }
+        })
+
+    }
+}
+
+if(typeof window !== 'undefined' && window.Vue){
+    //使用插件使用Vue.use()
+    window.Vue.use(MyPlugin)
+}
